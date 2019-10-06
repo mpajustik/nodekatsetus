ab0d6b0562aed0fd2908af20b0865746d48774f5
Merlis Pajustik <merlis.pajustik@gmail.com>
2019-10-07 01:26:37 +0300

    algus

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => resizeBy.send("Hello"));
+app.get("/", (req, res) => res.send("Hello"));
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3007;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
